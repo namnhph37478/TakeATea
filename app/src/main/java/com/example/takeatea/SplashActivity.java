9e6e589be088630f40e20f101dbd358bc1934c82
namnhph37478 <namnhph37478@fpt.edu.vn>
2025-07-16 08:22:20 +0700

    code màn hình chào

---
+++ app/src/main/java/com/example/takeatea/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.takeatea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 5000; // 5 giây
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            // Chuyển sang màn hình đăng nhập
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // kết thúc Splash
+        }, SPLASH_DELAY);
+    }
+}
